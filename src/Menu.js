09a81610b46a7beb7fecb816b3c8d045fb286ed8
Mateo Sam <sammateo4@gmail.com>
2021-05-30 12:57:00 -0400

    Trying to connect database

--- src/Menu.js
+++ src/Menu.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
 import MenuItem from "@components/MenuItem";
 export default function Menu() {
-	const [menuitems, setMenuItems] = useState(null);
+	const [menuitems, setMenuItems] = useState([
+		// { id: "1", name: "Jon" },
+		// { id: "2", name: "Jon" },
+	]);
 	async function getBase() {
 		const response = await fetch("/api/sql");
-		const obj = await response.json();
-		setMenuItems(obj);
-		// console.log(menuitems);
+		setMenuItems(await response.json());
+		console.log(menuitems);
 	}
 	useEffect(() => {
 		getBase();
 	}, []);
+
+	// function Call() {
+	// 	getBase();
+	// }
 	return (
 		<div>
-			{menuitems == null ? null : <p>{menuitems.name}</p>} <MenuItem></MenuItem>
+			{menuitems == null || menuitems.length < 1
+				? null
+				: menuitems.map((item) => <p key={item.id}>{item.name}</p>)}
+			<MenuItem></MenuItem>
+			{/* <button onClick={Call}>Click</button> */}
 		</div>
 	);
 }
